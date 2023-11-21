ac6d5b5d1b37e16f49ea548fe1cc1b2d3277fcf9
Lifeof31415926535 <tpray84@gmail.com>
2023-11-20 19:49:24 -0600

    Added custom DateTime format extensions.

---
+++ src/Application/Utilities/DateTimeExtensions.cs
@@ -0,0 +1,18 @@
+namespace Application.Utilities;
+
+public static class DateTimeExtensions
+{
+    public static string FormatTimestamp(this DateTime dateTime)
+    {
+        var currentDateTime = DateTime.UtcNow;
+        var timeSpan = currentDateTime - dateTime;
+
+        return timeSpan.TotalDays switch
+        {
+            0 => $"{timeSpan.TotalHours} hours ago",
+            1 => "yesterday",
+            < 21 => $"{timeSpan.TotalDays} days ago",
+            _ => dateTime.ToString("MMMM dd, yyyy")
+        };
+    }
+}
