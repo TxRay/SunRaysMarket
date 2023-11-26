a03267e9e7758dae58cd4452f827b17d61b42de6
Lifeof31415926535 <tpray84@gmail.com>
2023-11-25 18:28:04 -0600

    Created product card display with basic functionality.

--- src/Web/Endpoints/ComponentEndpoints.cs
+++ src/Web/Endpoints/ComponentEndpoints.cs
@@ -1,6 +1,7 @@
 using Application.DomainModels;
 using Application.UnitOfWork;
 using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
 using Web.Components;
 
 namespace Web.Endpoints;
@@ -15,6 +16,8 @@ public static class ComponentEndpoints
             .WithDescription("Endpoints which return HTML rendered by Blazor components.");
 
         componentGroup.MapProductDetailsComponentEndpoint();
+        componentGroup.MapCartControlEndpoints();
+        componentGroup.MapSignUpComponentEndpoints();
 
         return endpoints;
     }
@@ -32,6 +35,42 @@ public static class ComponentEndpoints
                 return new RazorComponentResult<ProductDetails>(new { Product = product });
             }
         );
+
+        return endpoints;
+    }
+
+    private static IEndpointRouteBuilder MapCartControlEndpoints(
+        this IEndpointRouteBuilder endpoints
+    )
+    {
+        endpoints.MapPost(
+            "/add-to-cart",
+            () => new RazorComponentResult<CartControls>(new { Quantity = 1 })
+        );
+
+        endpoints.MapPost(
+            "/quantity-control/{customerId:int}/{productId:int}/{idSuffix}",
+            (int customerId, int productId, string? idSuffix, [FromForm] int quantity) =>
+                new RazorComponentResult<QuantityControl>(new {Quantity=quantity, IdSuffix = idSuffix})
+            
+        );
+
+        return endpoints;
+    }
+    
+    private static IEndpointRouteBuilder MapSignUpComponentEndpoints(
+        this IEndpointRouteBuilder endpoints
+    )
+    {
+        endpoints.MapGet(
+            "/sign-up",
+            () => new RazorComponentResult<SignUp>()
+        );
+
+        endpoints.MapPost(
+            "/sign-up",
+            (SignupModel model) => new RazorComponentResult<SignUp>()
+            );
 
         return endpoints;
     }
--- src/Web/Pages/Index.cshtml.cs
+++ src/Web/Pages/Index.cshtml.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Application.DomainModels;
+using Application.UnitOfWork;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Web.Pages;
@@ -6,11 +8,20 @@ namespace Web.Pages;
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
-
-    public IndexModel(ILogger<IndexModel> logger)
+    private readonly IUnitOfWork _unitOfWork;
+    
+    public List<ProductListModel> FeaturedProducts { get; set; } = new();
+    
+    public IndexModel(ILogger<IndexModel> logger, IUnitOfWork unitOfWork)
     {
         _logger = logger;
+        _unitOfWork = unitOfWork;
     }
 
-    public void OnGet() { }
+    public async Task<IActionResult> OnGetAsync()
+    {
+        FeaturedProducts = (await _unitOfWork.ProductRepository.GetAllAsync()).ToList();
+        
+        return Page();
+    }
 }
