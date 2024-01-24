50e184ed7cb51e97b318960d366ee697cf14007d
Lifeof31415926535 <tpray84@gmail.com>
2024-01-23 19:27:17 -0600

    Created component to create page layouts

---
+++ Server/src/Server.Components/Enums/LayoutType.cs
@@ -0,0 +1,8 @@
+namespace SunRaysMarket.Server.Components.Enums;
+
+public enum LayoutType
+{
+    Admin,
+    Application,
+    Page
+}
---
+++ Server/src/Server.Components/Enums/Mappings/ContentWidthStyleMapping.cs
@@ -0,0 +1,12 @@
+namespace SunRaysMarket.Server.Components.Enums.Mappings;
+
+internal static class ContentWidthStyleMapping
+{
+    public static string ToStyleString(this ContentWidth contentWidth)
+        => contentWidth switch
+        {
+            ContentWidth.PageContent => "layout__content",
+            ContentWidth.PageFull => "layout__full",
+            _ => throw new ArgumentOutOfRangeException(nameof(contentWidth), contentWidth, null)
+        };
+}
