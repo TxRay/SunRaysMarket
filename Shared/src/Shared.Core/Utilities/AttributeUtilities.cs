d19ddc369af29dc4782ce991a06c9ac8360d378d
Lifeof31415926535 <tpray84@gmail.com>
2024-01-30 14:37:57 -0600

    Created custom input components for checkout options.

---
+++ Shared/src/Shared.Core/Utilities/AttributeUtilities.cs
@@ -0,0 +1,57 @@
+using System.Diagnostics.CodeAnalysis;
+
+namespace SunRaysMarket.Shared.Core.Utilities;
+
+public static class AttributeUtilities
+{
+    public static Dictionary<string, object> ConvertToDictionary(
+        IReadOnlyDictionary<string, object>? additionalAttributes,
+        IReadOnlyDictionary<string, IAttributeObject>? defaultAttributes)
+    {
+        var attributesDictionary = additionalAttributes switch
+        {
+            null => new Dictionary<string, object>(),
+            Dictionary<string, object> dict => dict,
+            _ => additionalAttributes.ToDictionary()
+        };
+
+        if (defaultAttributes is not null)
+        {
+            foreach (var (key, attributeObject) in defaultAttributes)
+            {
+                if (!attributesDictionary.TryAdd(key, attributeObject.CombineWithDefault(null)))
+                    attributesDictionary[key] = attributeObject.CombineWithDefault(attributesDictionary[key]);
+            }
+        }
+
+        return attributesDictionary;
+    }
+
+    public interface IAttributeObject
+    {
+        public object Value { get; }
+        public object CombineWithDefault(object? additionalValue);
+    }
+
+    public record AttributeObject<TValue>(
+        [NotNull] TValue ValueTyped,
+        Func<TValue, TValue, object>? RenderAsType = null) : IAttributeObject
+    {
+        public object Value => ValueTyped!;
+        public object CombineWithDefault(object? additionalValue)
+        {
+            var additionalValueTyped = additionalValue switch
+            {
+                TValue val => val,
+                _=> default
+            };
+
+            if (additionalValueTyped is null) return ValueTyped!;
+
+            return (RenderAsType is null)
+                ? additionalValueTyped
+                : RenderAsType.Invoke(ValueTyped, additionalValueTyped);
+        }
+        
+    }
+}
