de7e39b401faf5787203592b52f34d936000ca6e
Lifeof31415926535 <tpray84@gmail.com>
2024-04-04 15:52:44 -0500

    Allow  machines on local network to send request to dev server.

--- Server/src/Server.Web/Program.cs
+++ Server/src/Server.Web/Program.cs
@@ -66,9 +66,9 @@ else
     app.UseExceptionHandler("error/500/internal-server-error", true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
-    app.UseHttpsRedirection();
 }
 
+app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseAuthentication();
 app.UseAuthorization();
