a42f198611bfb67c7cd82568c450c01830a8b06e
Lifeof31415926535 <tpray84@gmail.com>
2024-04-08 20:09:41 -0500

    Improve home page hero layout for large screens.

--- Server/src/Server.Web/Program.cs
+++ Server/src/Server.Web/Program.cs
@@ -63,7 +63,7 @@ if (app.Environment.IsDevelopment())
 }
 else
 {
-    app.UseExceptionHandler("error/500/internal-server-error", true);
+    app.UseExceptionHandler("/error/500/internal-server-error", true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
